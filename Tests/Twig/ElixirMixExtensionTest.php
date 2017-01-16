a3523112f44a0c26b668dcc4dbfe46bfac0a2296
Iulian Popa <iulyanpopa@gmail.com>
2017-01-16 14:30:37 +0200

    Add tests for MixExtension

---
+++ Tests/Twig/ElixirMixExtensionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Iulyanp\ElixirMixBundle\Tests\Twig;
+
+use Iulyanp\ElixirMixBundle\Twig\MixExtension;
+
+/**
+ * Class Test
+ */
+class ElixirMixExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    const WEB_DIR = __DIR__.'/stub';
+
+    /**
+     * @var
+     */
+    private $mixExtension;
+
+    /**
+     * Set UP.
+     *
+     * Instantiate ElixirExtension for all tests
+     */
+    public function setUp()
+    {
+        $this->mixExtension = new MixExtension(self::WEB_DIR);
+    }
+
+    /**
+     * @test
+     */
+    public function mixFunctionReturnManifest()
+    {
+        $this->assertEquals(
+            'css/app-db9165hf67.css',
+            $this->mixExtension->mix('app.css')
+        );
+        $this->assertEquals(
+            'css/index-9rt53c9u67.css',
+            $this->mixExtension->mix('index.css')
+        );
+        $this->assertEquals(
+            'js/app-db9183c967.js',
+            $this->mixExtension->mix('app.js')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function elixirFunctionThrowsErrorWhenFileNotExists()
+    {
+        $this->expectException('\Exception');
+        $this->expectExceptionMessage('The "css/not_existing.css" key could not be found in the manifest file. '.
+        'Please pass just the asset filename as a parameter to the mix() method.');
+        $this->mixExtension->mix('css/not_existing.css');
+    }
+}
